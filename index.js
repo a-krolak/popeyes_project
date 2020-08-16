6c6acd7fd39ba7d59448d364882050e7abb2ffa6
Alex Krolak <akrolak@rbi.com>
2020-08-16 16:28:17 -0400

    set up clicks endpoint

--- index.js
+++ index.js
@@ -5,6 +5,13 @@ const express = require('express');
 const app = express();
 
 app.use(express.static('public'));
+app.use(express.json());
+
+app.post('/clicks', async function(request, response) {
+  const { clickX, clickY, tracking, target, time, id } = request.body;
+
+  response.json({ click: 'tracked' });
+});
 
 app.listen(PORT, () =>
   console.log(`Server is up and running at port ${PORT} 🚀`)
